692e421de957028fc64061c6a77739f2425c0ed0
RacimRgh <racim458@gmail.com>
2021-02-03 16:59:33 +0100

    version 4

--- Src/TP1_ex1_student.s
+++ Src/TP1_ex1_student.s
@@ -1,4 +1,5 @@
 .data
+VALEUR_SUP30: .byte 0
 SUM: .2byte 0
 RES: .2byte 0
 TAB: .4byte 1, 12, 28, 4, 3
@@ -23,11 +24,20 @@ loop:
 	Si ==20 on arrête le parcours et on fait un branchement vers end, sinon on fait la somme
 	*/
    cmp r1, #20
-   beq end
+   beq sup30
    ldr r3, [r0, r1]
    add r4, r4, r3
    add r1, r1, #4
    b loop
+   /* Vérification si la somme >= 30 */
+sup30:
+   cmp r4, #30
+   /* Si <30 on change pas la variable car elle est déjà initialisée à 0*/
+   bcc end
+   /* Chargement et modification de la variable */
+   ldr r6, =VALEUR_SUP30
+   mov r7, #1
+   str r7, [r6]
    /* Mettre le résultat de la somme dans la variable RES */
 end:
    str R4, [R5]
