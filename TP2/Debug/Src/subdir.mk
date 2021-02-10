1f826c33af2a2f3851e9160acb6ee7868bb28c27
RacimRgh <racim458@gmail.com>
2021-02-10 19:34:07 +0100

    exo2-2 non fini

--- TP2/Debug/Src/subdir.mk
+++ TP2/Debug/Src/subdir.mk
@@ -3,29 +3,25 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-S_SRCS += \
-../Src/TP2_ex1.s 
-
 C_SRCS += \
+../Src/TP2_ex2_1_student.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/TP2_ex1.o \
+./Src/TP2_ex2_1_student.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
-S_DEPS += \
-./Src/TP2_ex1.d 
-
 C_DEPS += \
+./Src/TP2_ex2_1_student.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/TP2_ex1.o: ../Src/TP2_ex1.s
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Src/TP2_ex1.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+Src/TP2_ex2_1_student.o: ../Src/TP2_ex2_1_student.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/TP2_ex2_1_student.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/syscalls.o: ../Src/syscalls.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/syscalls.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/sysmem.o: ../Src/sysmem.c
