30b905e26b219cb6149c7157f898c111ce9ec1c3
RacimRgh <racim458@gmail.com>
2021-02-17 17:10:44 +0100

    exo2-2 C+assembleur

--- TP2/Src/TP2_ex2_1_student.c
+++ TP2/Src/TP2_ex2_1_student.c
@@ -5,14 +5,15 @@
 
 int fibo(int x, int y);
 int fibo_suite[TAB_SIZE]={0};
-void fibo_asm(int x, int y);
+void fibo_asm();
+int x=0, y=1;
 
 
 int main (void){
 	
 	// Perform call to fibo function 
-	int res = fibo(0,1);
-	fibo_asm(0,1);
+	//int res = fibo(0,1);
+	fibo_asm(x,y);
 	
 	//Infinite loop
 	while(1){
@@ -36,28 +37,28 @@ int fibo(int x, int y){
 	return y;
 }
 
-void fibo_asm(int x, int y){
-	//A completer
-//	int res = 0;
-//	__asm ("ADD %[result], %[input_x], %[input_y]"
-//	    : [result] "=r" (res)
-//	    : [input_x] "r" (x), [input_y] "r" (y)
-//	);
-	asm (
-	    "TST LR, #0x40\n\t"
-	    "BEQ from_nonsecure\n\t"
-	  "from_secure:\n\t"
-	    "TST LR, #0x04\n\t"
-	    "ITE EQ\n\t"
-	    "MRSEQ R0, MSP\n\t"
-	    "MRSNE R0, PSP\n\t"
-	    "B hard_fault_handler_c\n\t"
-	  "from_nonsecure:\n\t"
-	    "MRS R0, CONTROL_NS\n\t"
-	    "TST R0, #2\n\t"
-	    "ITE EQ\n\t"
-	    "MRSEQ R0, MSP_NS\n\t"
-	    "MRSNE R0, PSP_NS\n\t"
-	    "B hard_fault_handler_c\n\t"
-	  );
+void fibo_asm(){
+	//Adresse du tableau
+	asm("LDR r0, =fibo_suite");
+	// Récupération de x et y et initialisation du compteur (debut:2*4 octets)
+	asm("MOV r1,#8\n\t"
+		"LDR r2, =x\n\t"
+		"LDR r3, =y\n\t"
+		"LDR r2, [r2]\n\t"
+		"LDR r3, [r3]\n\t"
+	);
+	// Stocakge des 2 premières valeurs du tableau
+	asm("STR r2,[r0]\n\t"
+		"STR r3,[r0,#4]\n\t");
+	// Effectuer la somme de fibonacci
+	asm("loop:\n\t"
+		"ADD r4,r3,r2\n\t"
+		"MOV r2,r3\n\t"
+		"MOV r3,r4\n\t"
+		"STR r4,[r0,r1]\n\t"
+		"ADD r1,r1,#4\n\t"
+		"CMP r1,#40\n\t"
+		"BNE loop\n\t"
+	);
 }
+
